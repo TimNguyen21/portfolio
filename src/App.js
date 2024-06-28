c6a365c7f49752221aaf7e72d4dd5e67eb85900c
TimNguyen21 <tim.nguyen2112@gmail.com>
2024-06-28 07:47:54 -0700

    [main] Temporary hide code to test deployment issues

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { Routes, Route } from "react-router-dom";
-import Main from './containers/main/Main.js';
-import Nav from './containers/nav/Nav.js';
-import Projects from './containers/projects/Projects.js';
-import Contact from './containers/contact/Contact.js';
+// import { Routes, Route } from "react-router-dom";
+// import Main from './containers/main/Main';
+import Nav from './containers/nav/Nav';
+// import Projects from './containers/projects/Projects';
+// import Contact from './containers/contact/Contact';
 
 import './App.scss';
 
@@ -12,13 +12,13 @@ function App() {
       <header>
         <Nav />
       </header>
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Main />} />
         <Route index path="about" element={<Main />} />
         <Route path="projects" element={<Projects />} />
         <Route path="contact" element={<Contact />} />
         <Route path="*" element={<Main />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
