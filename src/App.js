9e160db3535504731544d2e8d6ee416157bf76b2
TimNguyen21 <tim.nguyen2112@gmail.com>
2024-06-28 07:57:27 -0700

    [main] Revert original code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-// import Main from './containers/main/Main';
-// import Nav from './containers/nav/Nav';
+import Main from './containers/main/Main';
+import Nav from './containers/nav/Nav';
 import Projects from './containers/projects/Projects';
 import Contact from './containers/contact/Contact';
 
@@ -10,14 +10,14 @@ function App() {
   return (
     <div className="App">
       <header>
-        {/* <Nav /> */}
+        <Nav />
       </header>
       <Routes>
         <Route path="/" element={<div>hello world</div>} />
-        {/* <Route index path="about" element={<Main />} /> */}
+        <Route index path="about" element={<Main />} />
         <Route path="projects" element={<Projects />} />
         <Route path="contact" element={<Contact />} />
-        {/* <Route path="*" element={<Main />} /> */}
+        <Route path="*" element={<Main />} />
       </Routes>
     </div>
   );
